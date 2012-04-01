368e1e8527fac7291ddcc57f1f2ddf476c5cedf3
Rollen Gomes <avelliance@gmail.com>
2012-04-01 17:01:21 +0800

    finish cucumber feature to create a single directory

--- features/step_definitions/make_filestructure_steps.rb
+++ features/step_definitions/make_filestructure_steps.rb
@@ -1,9 +1,17 @@
+Before do
+  @pwd = Dir.pwd
+  FileUtils.rm_rf(sandbox_directory)
+  FileUtils.mkdir(sandbox_directory)
+  Dir.chdir(sandbox_directory)
+end
+
 Given /^that a directory named index does not exist$/ do
   File.directory?('index').should be_false    
 end
 
 When /^directory\('index'\) is invoked$/ do
-  @lingo = IMakeFile::FileStructure.new.directory('index')
+  @builder = IMakeFile::FileStructure.new(FileUtils)
+  @builder.directory('index')
 end
 
 Then /^a folder named index should be created$/ do
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,9 @@
-require_relative '../../lib/imakleuuuuuuefile/file_structure.rb'
+require_relative '../../lib/imakefile/file_structure.rb'
+
+module DirectoryHelpers
+  def sandbox_directory
+    File.expand_path('../../../sandbox/', __FILE__)
+  end
+end
+
+World(DirectoryHelpers)
